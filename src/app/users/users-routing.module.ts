d3726ccd002207eb1d43dbabaf0abe0612b40bbc
nfailla <faillanicolas@hotmail.com>
2019-05-31 13:22:36 -0300

    Routeo del UserDetalleComponent

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserDetalleComponent } from './user-detalle/user-detalle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'detalle/:id', component: UserDetalleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
